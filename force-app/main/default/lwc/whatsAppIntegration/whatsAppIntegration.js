738a493d6b40523d5b4b4848d02ce90c1a11b65c
aneela-oad <aneela-oad@github.com>
2022-11-11 16:25:59 +0500

    WhatsApp Integration with LWC successfully created

--- force-app/main/default/lwc/whatsAppIntegration/whatsAppIntegration.js
+++ force-app/main/default/lwc/whatsAppIntegration/whatsAppIntegration.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
+import sendTemplateMessage from '@salesforce/apex/WhatsController.sendTemplateMsg'
+export default class WhatsAppIntegration extends LightningElement {
 
-export default class WhatsAppIntegration extends LightningElement {}
+    @api recordId; 
+    onClickSendMsgTemplate(){
+        sendTemplateMessage({contactId : this.recordId})
+        .then( result => {
+            window.alert("Sent!")
+        })
+        .catch( error => {
+            window.alert("Sending Msg failed :(")
+        })
+    }
+}
