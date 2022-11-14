2dc74c75c25dbafff80fe41641450d1e47855e13
aneela-oad <aneela-oad@github.com>
2022-11-14 17:49:32 +0500

    search functionality added in pokemon tracker app

--- force-app/main/default/lwc/pokemonCardList/pokemonCardList.js
+++ force-app/main/default/lwc/pokemonCardList/pokemonCardList.js
@@ -4,6 +4,8 @@ import getPokemons from '@salesforce/apex/PokemonDetails.getPokemons'
 
 export default class PokemonCardList extends LightningElement {
 
+    searchKeywords = ''
+
     //Pokemons --> It will store details of all the pokemons
     pokemons;
     //error ---> It will be used to show error if the detail is failed to be fetch in the component
@@ -14,9 +16,21 @@ export default class PokemonCardList extends LightningElement {
     // we are using the connectedCallback() function. It allows us to load the component after it is 
     // rendered in DOM on the browser
 
-   
+  
     connectedCallback(){
-        getPokemons()
+        this.loadPokemons(this.searchKeywords)
+    }
+
+    handleSearch(e){
+        this.searchKeywords = e.target.value    
+        this.loadPokemons(this.searchKeywords)
+        
+    }
+
+
+
+    loadPokemons(searchwords){
+        getPokemons({searchkey:searchwords} )
         .then( result => {
            this.pokemons = result;
            console.log(this.pokemons);
